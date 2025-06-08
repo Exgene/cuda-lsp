fc048a5da29d2288ec79b6583ba9a4f312b3503a
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-06-08 22:12:47 +0530

    chore: update main to use decode

--- main.go
+++ main.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"github.com/exgene/cuda-autocompletes/internal/rpc"
+	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Started the main file")
+	logger := getLogger("/home/kausthubh/GitHub/cuda-autocompletes/log.txt")
 	scanner := bufio.NewScanner(os.Stdin)
+	logger.Println("Started the main file")
 
 	scanner.Split(rpc.Split)
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handleMessage(msg)
+		msg := scanner.Bytes()
+		handleMessage(msg, logger)
 	}
 }
 
-func handleMessage(msg string) {
-	fmt.Println(msg)
+func handleMessage(msg []byte, logger *log.Logger) {
+	logger.Printf("Received msg:%v", msg)
+	method, content, err := rpc.DecodeMessage(msg)
+	if err != nil {
+		logger.Fatalf("Error while Decoding Message:\n %v", err)
+		return
+	}
+	logger.Printf("Parsed Successfully! method: %s, content: %s", method, string(content))
+}
+
+func getLogger(filename string) *log.Logger {
+	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("You fucked up")
+	}
+	return log.New(logFile, "[lsp==cuda]", log.Ldate|log.Ltime|log.Lshortfile)
 }
