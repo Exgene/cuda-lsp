62dc0904147218f308816b9255b379d15870a3a8
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-06-12 23:10:20 +0530

    feat: on document change structs

---
+++ internal/lsp/textdocument_didchange.go
@@ -0,0 +1,26 @@
+package lsp
+
+type DidChangeTextDocumentNotification struct {
+	Notification
+	Params DidChangeTextDocumentParams `json:"params"`
+}
+
+type DidChangeTextDocumentParams struct {
+	TextDocument   VersionedTextDocumentIdentifier  `json:"textDocument"`
+	ContentChanges []TextDocumentContentChangeEvent `json:"contentChanges"`
+}
+
+type TextDocumentContentChangeEvent struct {
+	Range Range  `json:"range"`
+	Text  string `json:"text"`
+}
+
+type Range struct {
+	Start Position `json:"start"`
+	End   Position `json:"end"`
+}
+
+type Position struct {
+	Line      int `json:"line"`
+	Character int `json:"character"`
+}
