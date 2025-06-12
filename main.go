9fdbf9006b4ea138fdf95913d4b3ec059bdf2377
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-06-12 23:11:31 +0530

    WIP: incrimental diffs resolution

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/exgene/cuda-autocompletes/internal/analysis"
 	"github.com/exgene/cuda-autocompletes/internal/lsp"
 	"github.com/exgene/cuda-autocompletes/internal/rpc"
 )
@@ -13,16 +14,18 @@ import (
 func main() {
 	logger := getLogger("/home/kausthubh/GitHub/cuda-autocompletes/log.txt")
 	scanner := bufio.NewScanner(os.Stdin)
+	state := analysis.NewState()
+
 	logger.Println("Started the main file")
 
 	scanner.Split(rpc.Split)
 	for scanner.Scan() {
 		msg := scanner.Bytes()
-		handleMessage(msg, logger)
+		handleMessage(msg, logger, state)
 	}
 }
 
-func handleMessage(msg []byte, logger *log.Logger) {
+func handleMessage(msg []byte, logger *log.Logger, state analysis.State) {
 	logger.Printf("Received msg:%s", string(msg))
 	method, content, err := rpc.DecodeMessage(msg)
 	if err != nil {
@@ -52,6 +55,27 @@ func handleMessage(msg []byte, logger *log.Logger) {
 		}
 		logger.Printf("Document URI %s", document.Params.TextDocument.URI)
 		logger.Printf("%s", document.Params.TextDocument.Text)
+		state.OpenDocument(document.Params.TextDocument.URI, document.Params.TextDocument.Text)
+	case "textDocument/didChange":
+		var document lsp.DidChangeTextDocumentNotification
+		if err := json.Unmarshal(content, &document); err != nil {
+			logger.Fatalf("Error while Decoding IntializeRequest: \n%v", err)
+		}
+		for _, change := range document.Params.ContentChanges {
+			logger.Printf("Recieved Diffs to update Document, Diffed Text :: %s", change.Text)
+			diff := analysis.NewDiff()
+
+			diff.StartRange.Character = change.Range.Start.Character
+			diff.EndRange.Line = change.Range.Start.Line
+			diff.StartRange.Character = change.Range.End.Character
+			diff.EndRange.Line = change.Range.End.Line
+			diff.Text = change.Text
+
+			state.ApplyDiffs(document.Params.TextDocument.URI, diff)
+		}
+		for _, v := range state.Documents {
+			logger.Printf("Diffs Updated, The new document state looks like: %s", v)
+		}
 	}
 }
 
